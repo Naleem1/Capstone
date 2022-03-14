48d60609c44c0128d8c8e72431c0d3f9f91fd2e8
Naleem1 <Naleem25@gmail.com>
2022-03-14 00:18:58 -0400

    adding get information methods

--- main_app/views.py
+++ main_app/views.py
@@ -9,3 +9,6 @@ class viewOfList(ListView):
 class ItemListView(ListView):
     model = ToDoItem
     template = 'main_app/todolist_list.html'
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        return super().get_context_data(**kwargs)
