9164353a1a572067ef250393473d8116a2fd7b75
Naleem1 <Naleem25@gmail.com>
2022-03-13 23:42:59 -0400

    reroute to homepage working

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,11 @@
 from django.views.generic import ListView   
-from .models import ToDoList 
+from .models import ToDoItem, ToDoList 
+from django.urls import reverse
 
 class viewOfList(ListView):
     model = ToDoList
     template = 'main_app/index.html'
+
+class ItemListView(ListView):
+    model = ToDoItem
+    template = 'main_app/todolist_list.html'
